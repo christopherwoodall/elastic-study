eab6bce96f0f1419f0420c7e8da150fa8dbec2bf
Chris <363708+christopherwoodall@users.noreply.github.com>
2026-05-26 16:41:14 -0500

    Update host logger

--- src/log_replay/main.py
+++ src/log_replay/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 import os
 
 from dotenv import load_dotenv
@@ -6,11 +7,18 @@ from elasticsearch import AsyncElasticsearch
 
 from log_replay.downloader import DatasetManager
 from log_replay.engine import LogReplayer
-from log_replay.host.mordor import MordorSysmonReader
-from log_replay.logger import logger
+from log_replay.host.azure_linux import AzureHostReader
+from log_replay.logging import logger
 from log_replay.network.zeek import ZeekConnReader
 
+# from log_replay.host.mordor import MordorSysmonReader
+
+
 load_dotenv()
+
+BASE_URL = os.getenv("REPLAY_LOG_BASE_URL")
+HOST_FILES = json.loads(os.getenv("REPLAY_LOG_HOST_FILES", "[]"))
+NETWORK_FILES = json.loads(os.getenv("REPLAY_LOG_NET_FILES", "[]"))
 
 
 async def run_replay():
@@ -19,33 +27,32 @@ async def run_replay():
     # Use standard HTTP and remove auth since xpack.security is disabled locally
     es = AsyncElasticsearch("http://localhost:9200")
 
-    manager = DatasetManager()
     target_index = os.getenv("REPLAY_INDEX", "logs-endpoint.events-simulated")
+
+    manager = DatasetManager()
     replayer = LogReplayer(es_client=es, target_index=target_index)
 
-    # 1. Host Attack Simulation (Fast-forward 5x)
-    host_file = await manager.fetch_dataset(
-        url="https://raw.githubusercontent.com/OTRF/Security-Datasets/master/datasets/atomic/windows/execution/host/empire_launcher_vbs.zip",
-        filename="empire_launcher_vbs.zip",
-    )
-    host_reader = MordorSysmonReader(host_file)
-    await replayer.replay(host_reader, speed_multiplier=5.0)
+    url_seperator = "" if BASE_URL.endswith("/") else "/"
 
-    # 2. Network Anomaly Simulation (Brimdata Generic Network Noise)
-    network_file_1 = await manager.fetch_dataset(
-        url="https://raw.githubusercontent.com/brimdata/zed-sample-data/main/zeek-json/conn.json.gz",
-        filename="conn_brim.json.gz",
-    )
-    network_reader_1 = ZeekConnReader(network_file_1)
-    await replayer.replay(network_reader_1, speed_multiplier=0, chunk_size=500)
+    for f in HOST_FILES:
+        logger.info(f"Processing host log file: {f}")
 
-    # 3. Network Anomaly Simulation (IoT-23 Mirai Botnet C2 Traffic)
-    network_file_2 = await manager.fetch_dataset(
-        url="https://mcfp.felk.cvut.cz/publicDatasets/IoT-23-Dataset/IndividualScenarios/CTU-IoT-Malware-Capture-1-1/bro/conn.log.labeled",
-        filename="conn_iot23.log.labeled",
-    )
-    network_reader_2 = ZeekConnReader(network_file_2)
-    await replayer.replay(network_reader_2, speed_multiplier=0, chunk_size=500)
+        log_file = await manager.fetch_dataset(
+            url=f"{BASE_URL}{url_seperator}{f}", filename=f
+        )
+        reader = AzureHostReader(log_file)
+        # Fast-forward host events at 5x speed to quickly simulate an attack scenario
+        await replayer.replay(reader, speed_multiplier=5.0)
+
+    # for f in NETWORK_FILES:
+    #     logger.info(f"Processing network log file: {f}")
+
+    #     log_file = await manager.fetch_dataset(
+    #         url=f"{BASE_URL}{url_seperator}{f}", filename=f
+    #     )
+    #     reader = ZeekConnReader(log_file)
+    #     # Ingest network events at full speed (0 multiplier) in larger chunks to simulate high-throughput scenarios
+    #     await replayer.replay(reader, speed_multiplier=0, chunk_size=500)
 
     await es.close()
 
